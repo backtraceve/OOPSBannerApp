48adfe96a27fb4a7092982bf3f85fdbaeb767cdc
backtraceve <evelinap007@gmail.com>
2026-03-01 20:46:38 +0530

    Add files via upload

---
+++ OOPSBannerApp/OOPSBannerApp.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerApp uc1 - OOPS Banner Display Aplication 
+ * 
+ * This class demonstrates a simple java aplication that displays the Object 
+ * Orieted Programming System OOPS acronym to the console.
+ * 
+ * @author Developer
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
